410607373a90becd2679c1cfeccbd69aac2b5009
Kavia AI <production@kavia.ai>
2026-02-09 10:27:22 +0000

    Checkpoint 1 - cg62264940

---
+++ flight_status_service/src/main/java/com/example/demo/flightstatusserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.flightstatusservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class flightstatusserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(flightstatusserviceApplication.class, args);
+	}
+
+}
